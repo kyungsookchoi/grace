531e3822d25872450222b5adebe6e066df3376f8
kyungsookchoi <gracecks1977@gmail.com>
2025-09-25 19:55:40 +0900

    axios
    axios

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,9 +4,8 @@
     <div>
       Board List:
       <div v-if="loading">Loading...</div>
-      <div v-else>
-        Api result: {{ apiRes }}
-      </div>
+      <div v-else>Api result: <pre>{{ apiRes }}</pre></div>
+      <div v-if="error"><pre>{{ error }}</pre></div>
       <ul>
         <li>
           <router-Link to="/b/1">Board 1</router-Link>
@@ -19,11 +18,14 @@
   </div>
 </template>
 <script>
+import axios from 'axios'
+
 export default{
   data() {
     return {
       loading: false,
       apiRes: '',
+      error: '',
     }
   },
   created(){
@@ -32,20 +34,16 @@ export default{
   methods: {
     fetchData(){
       this.loading = true 
-
-      const req = new XMLHttpRequest()
-
-      req.open('GET', 'http://localhost:3000/health')
-
-      req.send()
-
-      req.addEventListener('load', () => {
+ 
+      axios.get('http://localhost:3000/_health')
+      .then(res => {
+        this.apiRes = res.data
+      })
+      .catch(res => {
+        this.error = res.response.data
+      })
+      .finally(() => {
         this.loading = false
-        this.apiRes = {
-          status: req.status,
-          statusText: req .statusText,
-          response: JSON.parse(req.response)
-        }
       })
     }
   }
