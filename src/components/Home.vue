d6980db66274d7081834cf0502fe0d2ebbb08116
kyungsookchoi <gracecks1977@gmail.com>
2025-09-25 17:50:35 +0900

    Ajax - HttpXMLRequst객체
    Ajax - HttpXMLRequst객체

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,10 @@
     Home
     <div>
       Board List:
+      <div v-if="loading">Loading...</div>
+      <div v-else>
+        Api result: {{ apiRes }}
+      </div>
       <ul>
         <li>
           <router-Link to="/b/1">Board 1</router-Link>
@@ -16,7 +20,35 @@
 </template>
 <script>
 export default{
+  data() {
+    return {
+      loading: false,
+      apiRes: '',
+    }
+  },
+  created(){
+    this.fetchData()
+  },
+  methods: {
+    fetchData(){
+      this.loading = true 
 
+      const req = new XMLHttpRequest()
+
+      req.open('GET', 'http://localhost:3000/health')
+
+      req.send()
+
+      req.addEventListener('load', () => {
+        this.loading = false
+        this.apiRes = {
+          status: req.status,
+          statusText: req .statusText,
+          response: JSON.parse(req.response)
+        }
+      })
+    }
+  }
 }
 </script>
 
